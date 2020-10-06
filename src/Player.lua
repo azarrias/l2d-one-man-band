4926edfbebf81078066ba3988d4469842888677d
azarrias <adolfo.zarrias@gmail.com>
2020-10-06 10:17:16 +0200

    Factor kinetic friction and impulse force into player's movement

--- src/Player.lua
+++ src/Player.lua
@@ -3,15 +3,13 @@ Player = Class{}
 function Player:init(level)
   self.level = level
   self.world = level.world
-  self.kinetic_friction = 150
-  self.velocity_coeficient = -360
-  self.velocity = tiny.Vector2D(0, 0)
-  self.position = tiny.Vector2D(math.floor(VIRTUAL_SIZE.x / 2), math.floor(VIRTUAL_SIZE.y / 2))
+  self.kinetic_friction = 3
+  self.impulse_coeficient = -180
   
-  self.body = love.physics.newBody(self.world, self.position.x, self.position.y, "dynamic")
+  self.body = love.physics.newBody(self.world, math.floor(VIRTUAL_SIZE.x / 2), math.floor(VIRTUAL_SIZE.y / 2), "dynamic")
 	self.shape = love.physics.newCircleShape(math.floor(VIRTUAL_SIZE.x / 50))
 	self.fixture = love.physics.newFixture(self.body, self.shape)
-  self.fixture:setRestitution(0.9)
+  self.fixture:setRestitution(1)
   
   self.ray_vector = nil
   self.ray_max_length = math.sqrt(math.pow(VIRTUAL_SIZE.x, 2) + math.pow(VIRTUAL_SIZE.y, 2))
@@ -20,7 +18,7 @@ end
 function Player:update(dt)
   local vel_x, vel_y = self.body:getLinearVelocity()
   if math.abs(vel_x) > 5 or math.abs(vel_y) > 5 then
-    self.body:setLinearVelocity(vel_x / 1.25, vel_y / 1.25) --dt
+    self.body:setLinearVelocity(vel_x - vel_x * self.kinetic_friction * dt, vel_y - vel_y * self.kinetic_friction * dt)
   else
     self.body:setLinearVelocity(0, 0)
   end
@@ -40,8 +38,8 @@ end
 function Player:Dash()
   self.ray_vector = nil
   local mouse_x, mouse_y = push:toGame(love.mouse.getPosition())
-  self.velocity = tiny.Vector2D(mouse_x - self.body:getX(), mouse_y - self.body:getY()):Normalize() * self.velocity_coeficient 
-  self.body:applyLinearImpulse(self.velocity.x, self.velocity.y)
+  local impulse = tiny.Vector2D(mouse_x - self.body:getX(), mouse_y - self.body:getY()):Normalize() * self.impulse_coeficient 
+  self.body:applyLinearImpulse(impulse.x, impulse.y)
 end
 
 function Player:Shoot()
